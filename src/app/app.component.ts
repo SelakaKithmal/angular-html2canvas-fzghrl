f7b423f2cf885018693682188987c1ebfda5fba6
SelakaKithmal <selaka.kithmal@gmail.com>
2022-12-29 08:12:22 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-//import * as html2canvas from 'html2canvas';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  someArray = [];
-  downloadImage(){
+export class AppComponent {
+  private someArray: any[][];
 
-    this.someArray.push(['b','f']);
-    
-    console.log(JSON.stringify(this.someArray))
+  constructor() {
+    this.someArray = [];
+    this.printArray()
+  }
+
+  printArray() {
+    for (var i: number = 0; i < 10; i++) {
+      this.someArray[i] = [];
+      for (var j: number = 0; j < 2; j++) {
+        if (j == 1) {
+          this.someArray[i][j] = 'XXX';
+        } else {
+          this.someArray[i][j] = 'ZZZ';
+        }
+      }
+    }
+    console.log(JSON.stringify(this.someArray));
   }
 }
